c518b360064572f320786d6e5a474ccc67428630
Viraj Lakshitha Bandara <62491019+viraj-lakshitha@users.noreply.github.com>
2022-05-15 17:26:01 +0530

    Create basic UI to add task

--- TaskAssist/ContentView.swift
+++ TaskAssist/ContentView.swift
@@ -7,10 +7,61 @@
 
 import SwiftUI
 
+enum Priority: String, Identifiable, CaseIterable {
+    var id: UUID {
+        return UUID()
+    }
+    case low = "Low"
+    case medium = "Medium"
+    case high = "High"
+}
+
+extension Priority {
+    var title: String {
+        switch self {
+        case .low:
+            return "Low"
+        case .medium:
+            return "Medium"
+        case .high:
+            return "High"
+        }
+    }
+}
+
 struct ContentView: View {
+    
+    @State var title: String = ""
+    @State var domain: String = ""
+    @State var priority: Priority = .low
+    
     var body: some View {
-        Text("Hello, world!")
+        NavigationView {
+            VStack {
+                TextField("Enter Title", text: $title)
+                    .textFieldStyle(.roundedBorder)
+                TextField("Enter Domain", text: $domain)
+                    .textFieldStyle(.roundedBorder)
+                Picker("Priority", selection: $priority) {
+                    ForEach(Priority.allCases) {
+                        priority in Text(priority.title).tag(priority)
+                    }
+                }.pickerStyle(.segmented)
+                
+                Button("Save") {
+                    
+                }
+                .padding(10)
+                .frame(maxWidth: .infinity)
+                .background(Color.blue)
+                .foregroundColor(Color.white)
+                .clipShape(RoundedRectangle(cornerRadius: 10.0, style: .continuous))
+                
+                Spacer()
+            }
             .padding()
+            .navigationTitle("Task Assits")
+        }
     }
 }
 
